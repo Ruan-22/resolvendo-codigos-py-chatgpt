346c469a93deb03ba351ece0258a9aa52c706441
Ruan-22 <ruancar500@gmail.com>
2025-12-16 15:34:31 -0300

    Adicionando os arquivos py

---
+++ concatenando_dados.py
@@ -0,0 +1,8 @@
+# Receber duas strings diferentes e concatena-los em uma única string
+
+string1 = input("Digite a primeira string: ")
+string2 = input("Digite a segunda string: ")
+
+resultado = string1 + " " + string2
+
+print(f"String concatenada: {resultado}")
