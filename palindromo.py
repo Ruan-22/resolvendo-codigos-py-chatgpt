f451407794083057544391face9a980ecef8f7d2
Ruan-22 <ruancar500@gmail.com>
2025-12-16 15:40:12 -0300

    Adicionando os arquivos py

---
+++ palindromo.py
@@ -0,0 +1,10 @@
+# Testar se a string é um palíndromo7
+
+texto = input("Digite uma palavra ou frase: ")
+
+texto_formatado = texto.replace(" ", "").lower()
+
+if texto_formatado == texto_formatado[::-1]:
+    print(f'"{texto}" é um palíndromo.')
+else:
+    print(f'"{texto}" não é um palíndromo.')
